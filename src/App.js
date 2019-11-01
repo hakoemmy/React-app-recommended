053c7e397deeebc4ea0794d4a952e3ccac8a11e4
H.Emmanuel <hakoemmy.2017@gmail.com>
2019-11-01 10:42:59 +0200

    Add Person component Add State Add event listener

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+
 
 class App extends Component {
+  state = {
+     persons: [
+      {
+         name: 'Emmanuel',
+         age: 23
+      },
+      {
+          name: 'Victor',
+          age: 22
+      },
+       {
+          name: 'Francoise',
+          age: 21
+        }
+      ]
+    };
+
+    switchNameHandler = () =>{
+      
+      this.setState({
+        persons: [
+        {
+           name: 'Peter',
+           age: 24
+        },
+        {
+            name: 'Victor',
+            age: 22
+        },
+         {
+            name: 'Francoise',
+            age: 22
+          }
+        ]})
+      
+    }
   render() {
     return (
-      <div className="App">
-        <h1>This is React app</h1>
-      </div>
+    <div className="App">
+       <h1>This is React app</h1>
+       <p>I am testing!</p>
+       <button onClick={this.switchNameHandler}>Swicth Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> and I like wathing movies</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+    </div>
     );
   }
 }

c051a9ff48b22c8999911a608ddfe01f98811484
H.Emmanuel <hakoemmy.2017@gmail.com>
2019-11-12 13:07:07 +0200

    Add many components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Person.css';
+
 const person = (props) =>{
+
     return(
         <div className="Person"> 
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>

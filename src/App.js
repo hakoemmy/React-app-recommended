30f43e1f41e00c5fd98556518344fd85566b9a67
Emmanuel HAKORIMANA <51243858+hakoemmy@users.noreply.github.com>
2019-11-12 14:48:52 +0200

    Add pseudo selectors access (#7)
    Add Css media queries access

--- src/App.js
+++ src/App.js
@@ -45,16 +45,9 @@ class App extends Component {
   }
 
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-    };
-
+  
     let persons = null;
-
+    let btnClass = null;
     if ( this.state.showPersons ) {
       persons = (
         <div>
@@ -68,6 +61,7 @@ class App extends Component {
           } )}
         </div>
       );
+      btnClass = classes.Red;
     }
 
     const assignedClasses = [];
@@ -83,7 +77,7 @@ class App extends Component {
           <h1>Hi, I'm a React App</h1>
           <p className={assignedClasses.join( ' ' )}>This is really working!</p>
           <button
-            style={style}
+            className={btnClass}
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>

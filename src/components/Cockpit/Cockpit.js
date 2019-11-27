bf78078a4c4e24f2ec52a792e9600d4852f97bfa
H.Emmanuel <hakoemmy.2017@gmail.com>
2019-11-27 15:45:28 +0200

    Dive into React

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 
 import classes from '../Cockpit/CockPit.css';
-
+import AuthContext from '../../context/authContext';
+import Aux from '../../hoc/Auxilliary';
 const cockPit = (props) => {
+  const toogleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+  useEffect(() => {
+   console.log('[CockPit.js] useEffect');
+    // setTimeout(()=> {
+    //   alert('Saved data to cloud!');
+    // }, 1000);
+    toogleBtnRef.current.click();
+   // combines use of componentDidMount
+   // and componentDidUpdate
+  },[]);
     const assignedClasses = [];
     let btnClass = null;
-    if ( props.persons.length <= 2 ) {
+    if ( props.personsLength <= 2 ) {
       assignedClasses.push( classes.red ); // classes = ['red']
     }
-    if ( props.persons.length <= 1 ) {
+    if ( props.personsLength <= 1 ) {
       assignedClasses.push( classes.bold); // classes = ['red', 'bold']
     }
 
@@ -18,15 +30,34 @@ const cockPit = (props) => {
 
   return (
     <div className={classes.CockPit}>
-    <h1>Hi, I'm a React App</h1>
+    <h1>{props.title}</h1>
     <p className={assignedClasses.join( ' ' )}>This is really working!</p>
     <button
+      ref={toogleBtnRef}
       className={btnClass}
       onClick={props.toggle}>Toggle Persons</button>
+       {/* <AuthContext.Consumer>
+        { context => (
+          <Aux>
+        <button onClick={context.login}>Log in</button>
+        <button onClick={context.logout}>Log out</button>
+        </Aux>
+        )
+        }
+       </AuthContext.Consumer> */}
+
+       {  (
+          <Aux>
+        <button onClick={authContext.login}>Log in</button>
+        <button onClick={authContext.logout}>Log out</button>
+        </Aux>
+        )
+        }
+        
   </div>
   );
 };
 
 
 
-export default cockPit;
+export default React.memo(cockPit);
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.css';
-const person = (props) =>{
-    return(
-        <div className= {classes.Person}> 
-            <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-   
-    );
+import Aux from '../../../hoc/Auxilliary';
+import withClass from '../../../hoc/withClass';
+import AuthContext from '../../../context/authContext';
+class Person extends Component{
+    constructor(props){
+        super(props);
+        this.inputElRef = React.createRef();
+    }
+    componentDidMount(){
+        this.inputElRef.current.focus();
+    }
+    static contextType = AuthContext;
+    render(){
+        console.log('[Person.js] rendering....');
+        return(
+            <Aux>
+                {/* <AuthContext.Consumer>
+                  { context => 
+                       context.authenticated? <p>authenticated!</p>: <p>Please login</p>      
+                  }  
+                </AuthContext.Consumer> */}
+                    { 
+                    this.context.authenticated? <p>authenticated!</p>: <p>Please login</p>
+                    }
+                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input type="text" 
+                 ref={this.inputElRef}
+                onChange={this.props.changed}
+                value={this.props.name} />
+            </Aux>
+       
+        );
+    }
+    
 };
-
-export default person;
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
+export default withClass(Person,classes.Person);
